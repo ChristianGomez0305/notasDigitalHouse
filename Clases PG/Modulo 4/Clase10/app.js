1a9dcaa1a5cbfedc6dde3424f2f29b5a889f1eb6
ChristianGomez0305 <christian990503@hotmail.com>
2021-07-22 21:08:20 -0500

    Clase 10

--- Clases PG/Modulo 4/Clase10/app.js
+++ Clases PG/Modulo 4/Clase10/app.js
@@ -1,14 +1,26 @@
+const path = require('path');
 const express = require('express');
+const { dirname } = require('path');
 const app = express();
 
 let saludo = "Bienvenido/a, ahora estamos en tu perfil"
 
+// Usar recursos estaticos
+app.use(express.static('public'));
+
 // Poner a correr el servidor, el puero sera el 3000 y retorna mensaje de exito
 app.listen(3000, () => console.log('Servidor Corriendo'))
 
-// Indica que al hacer una peticion de get / imprima el mensaje
+// Indica que al hacer una peticion de get con una ruta ingresar a cierta ruta
+
 app.get('/', function(req,res){
-    res.send("Bienvenido al sitio");
+    let htmlPath = path.resolve(__dirname, './views/index.html');
+    res.sendFile(htmlPath);
+})
+
+app.get('/index', function(req,res){
+    let htmlPath = path.join(__dirname, '/views/index.html');
+    res.sendFile(htmlPath);
 })
 
 app.get('/contacto', function(req,res){
